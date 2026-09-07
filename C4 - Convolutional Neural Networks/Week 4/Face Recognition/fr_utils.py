5bddef07e472b4e7e87b5a057e92a5dcffb50576
nevilleajim <nevilleajim@gmail.com>
2026-09-07 12:01:16 +0100

    Sequence Model Week 1, Assignment 1

--- C4 - Convolutional Neural Networks/Week 4/Face Recognition/fr_utils.py
+++ C4 - Convolutional Neural Networks/Week 4/Face Recognition/fr_utils.py
@@ -3,7 +3,6 @@
 import tensorflow as tf
 import numpy as np
 import os
-#import cv2
 from numpy import genfromtxt
 from tensorflow.keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
 from tensorflow.keras.models import Model
@@ -14,8 +13,9 @@ import matplotlib.pyplot as plt
 import PIL
 from tensorflow.keras import backend as K
 
-K.set_image_data_format('channels_first')
-
+# ====================== CHANGED ======================
+K.set_image_data_format('channels_last')
+# =====================================================
 
 _FLOATX = 'float32'
 
@@ -36,7 +36,7 @@ def zeros(shape, dtype=_FLOATX, name=None):
 def concatenate(tensors, axis=-1):
     if axis < 0:
         axis = axis % len(tensors[0].get_shape())
-    return tf.concat(axis, tensors)
+    return tf.concat(tensors, axis)          # modern tf.concat order
 
 def LRN2D(x):
     return tf.nn.lrn(x, alpha=1e-4, beta=0.75)
@@ -51,17 +51,24 @@ def conv2d_bn(x,
               cv2_strides=(1, 1),
               padding=None):
     num = '' if cv2_out == None else '1'
-    tensor = Conv2D(cv1_out, cv1_filter, strides=cv1_strides, data_format='channels_first', name=layer+'_conv'+num)(x)
-    tensor = BatchNormalization(axis=1, epsilon=0.00001, name=layer+'_bn'+num)(tensor)
+    
+    # ====================== CHANGED ======================
+    tensor = Conv2D(cv1_out, cv1_filter, strides=cv1_strides, name=layer+'_conv'+num)(x)
+    tensor = BatchNormalization(axis=-1, epsilon=0.00001, name=layer+'_bn'+num)(tensor)
     tensor = Activation('relu')(tensor)
+    
     if padding == None:
         return tensor
-    tensor = ZeroPadding2D(padding=padding, data_format='channels_first')(tensor)
+    
+    tensor = ZeroPadding2D(padding=padding)(tensor)
+    
     if cv2_out == None:
         return tensor
-    tensor = Conv2D(cv2_out, cv2_filter, strides=cv2_strides, data_format='channels_first', name=layer+'_conv'+'2')(tensor)
-    tensor = BatchNormalization(axis=1, epsilon=0.00001, name=layer+'_bn'+'2')(tensor)
+    
+    tensor = Conv2D(cv2_out, cv2_filter, strides=cv2_strides, name=layer+'_conv'+'2')(tensor)
+    tensor = BatchNormalization(axis=-1, epsilon=0.00001, name=layer+'_bn'+'2')(tensor)
     tensor = Activation('relu')(tensor)
+    # =====================================================
     return tensor
 
 WEIGHTS = [
@@ -113,7 +120,7 @@ conv_shape = {
   'inception_3c_5x5_conv2': [64, 32, 5, 5],
   'inception_4a_3x3_conv1': [96, 640, 1, 1],
   'inception_4a_3x3_conv2': [192, 96, 3, 3],
-  'inception_4a_5x5_conv1': [32, 640, 1, 1,],
+  'inception_4a_5x5_conv1': [32, 640, 1, 1],
   'inception_4a_5x5_conv2': [64, 32, 5, 5],
   'inception_4a_pool_conv': [128, 640, 1, 1],
   'inception_4a_1x1_conv': [256, 640, 1, 1],
@@ -132,19 +139,14 @@ conv_shape = {
 }
 
 def load_weights_from_FaceNet(FRmodel):
-    # Load weights from csv files (which was exported from Openface torch model)
     weights = WEIGHTS
     weights_dict = load_weights()
 
-    # Set layer weights of the model
     for name in weights:
-        if FRmodel.get_layer(name) != None:
+        if FRmodel.get_layer(name) is not None:
             FRmodel.get_layer(name).set_weights(weights_dict[name])
-        elif Model.get_layer(name) != None:
-            Model.get_layer(name).set_weights(weights_dict[name])
 
 def load_weights():
-    # Set weights path
     dirPath = './weights'
     fileNames = filter(lambda f: not f.startswith('.'), os.listdir(dirPath))
     paths = {}
@@ -157,9 +159,10 @@ def load_weights():
         if 'conv' in name:
             conv_w = genfromtxt(paths[name + '_w'], delimiter=',', dtype=None)
             conv_w = np.reshape(conv_w, conv_shape[name])
+            # This transpose converts [out, in, H, W] → [H, W, in, out] (correct for channels_last)
             conv_w = np.transpose(conv_w, (2, 3, 1, 0))
             conv_b = genfromtxt(paths[name + '_b'], delimiter=',', dtype=None)
-            weights_dict[name] = [conv_w, conv_b]     
+            weights_dict[name] = [conv_w, conv_b]
         elif 'bn' in name:
             bn_w = genfromtxt(paths[name + '_w'], delimiter=',', dtype=None)
             bn_b = genfromtxt(paths[name + '_b'], delimiter=',', dtype=None)
@@ -178,26 +181,30 @@ def load_weights():
 
 def load_dataset():
     train_dataset = h5py.File('datasets/train_happy.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:])
 
     test_dataset = h5py.File('datasets/test_happy.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:])
 
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    classes = np.array(test_dataset["list_classes"][:])
     
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
     
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
+
 def img_to_encoding(image_path, model):
-    #img = PIL.Image.open(image_path)
-    img = tf.keras.preprocessing.image.load_img(image_path)
-    #img = img1[...,::-1]
-    img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
-    x_train = np.expand_dims(img, axis=0)
-    print(x_train.shape)
+    img = tf.keras.preprocessing.image.load_img(image_path, target_size=(96, 96))
+    img = np.array(img)
+    
+    # ====================== CHANGED ======================
+    # For channels_last we keep (H, W, C) — do NOT transpose
+    img = np.around(img / 255.0, decimals=12)
+    # =====================================================
+    
+    x_train = np.expand_dims(img, axis=0)          # shape: (1, 96, 96, 3)
     embedding = model.predict_on_batch(x_train)
     return embedding
